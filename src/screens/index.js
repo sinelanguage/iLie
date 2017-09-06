9da1931c9f3a5026cfb88c860b581a6c110b4441
Adam Winick <sinelanguage@gmail.com>
2017-09-06 19:41:30 -0400

    moved getStore functions to button of screens index

--- src/screens/index.js
+++ src/screens/index.js
@@ -5,9 +5,6 @@ import configureStore from '../store/configure.store';
 import { initalState } from '../store/initial.state';
 import { intialActionCreator } from '../actions/initial.action';
 
-export const store = configureStore(initalState);
-export const getStore = () => console.log('STORE: ', store.getState(), store);
-
 import FirstTimeLaunch from './first-time/first-time.view';
 import Tour from './tour/tour.view';
 import RecordLie from './record-lie/record-lie.view';
@@ -22,3 +19,6 @@ export function registerScreens() {
   Navigation.registerComponent('view.reason', () => Reason, store, Provider);
   Navigation.registerComponent('view.login', () => Login, store, Provider);
 }
+
+export const store = configureStore(initalState);
+export const getStore = () => console.log('STORE: ', store.getState(), store);
