88492301478950f1990449113e1c99b68221229b
Adam Winick <sinelanguage@gmail.com>
2017-12-09 11:55:51 -0500

    grid lines

--- src/screens/first-time/grid-lines.view.js
+++ src/screens/first-time/grid-lines.view.js
@@ -6,18 +6,50 @@ import {
   Image,
 } from 'react-native';
 
-export const GridLines = () => (
-  <View style={styles.container}>
+export const GridLines = () => {
+  renderGroupOfTicks = () => {
+    const ticks = [];
+    for (let index = 0; index < 10; index++) {
+      ticks.push(<View key={index} style={styles.tick}></View>) 
+    };
+    ticks.push(<View key={index} style={styles.widerTick}></View>)
+    return ticks
+  }
+  renderWiderTicks = () => {
+    const ticks = [];
+    for (let index = 0; index < 100; index++) {
+      ticks.push(<View key={index} style={styles.tick}></View>) 
+    };
+    return ticks
+  }
 
-  </View>
-)
+  return (
+    <View style={styles.container}>
+      {renderTicks()} 
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'red',
     zIndex: 1,
     ...StyleSheet.absoluteFillObject,
-    width: 20,
+    width: 40,
+    flexDirection: 'column',
   },
+  tick: {
+    backgroundColor: 'white',
+    width: 20,
+    height: 1,
+    marginBottom: 4,
+    opacity: 0.3
+  },
+  widerTick: {
+    backgroundColor: 'white',
+    width: 40,
+    height: 1,
+    marginBottom: 4,
+    opacity: 0.3
+  }
 
 })
